53a2ec1e9ddd71d693d601a94ed9b7ca6207f855
jackymahc@gmail.com <jackyma1213@users.noreply.github.com>
2022-04-02 19:08:53 +0800

    feat: add storage

---
+++ storage/main.go
@@ -0,0 +1,25 @@
+package storage
+
+type Item struct {
+	FileId           int    `json:"fileId"`
+	Name             string `json:"name"`
+	ObjectType       int    `json:"objectType"`
+	ParentFieldId    int    `json:"parentField"`
+	LastModifiedDate string `json:"lastModifiedDate"`
+}
+
+type Db struct {
+	Items []Item
+}
+
+func New() *Db {
+	return &Db{}
+}
+
+func (db *Db) Add(item Item) {
+	db.Items = append(db.Items, item)
+}
+
+func (db *Db) Delete(fileId int) {
+
+}
